fcbabd7d18ded21aa59902d51f0d1307f6a26563
mvanzulli <mvanzulli@fing.edu.uy>
2023-02-13 14:34:54 -0300

    Added data_loader and Net (#7)

--- dataLoading.py
+++ dataLoading.py
@@ -14,6 +14,7 @@ import torch.optim as optim
 import time
 
 # Loading the data
+#echo "$Lx,$Ly,$Lz,$E,$nu,$p,$Ux,$Uy,$Uz" >> "$filename"
 class uniCompDataset(Dataset):
     def __init__(self, csv_file):
         self.data = pd.read_csv(csv_file, header=None)
@@ -25,105 +26,89 @@ class uniCompDataset(Dataset):
         return self.len
 
 # Creating the model
-class uniCompNet(nn.Module):
-    def __init__(self):
-        super(uniCompNet, self).__init__()
-        self.fc1 = nn.Linear(3, 10)
-        self.fc2 = nn.Linear(10, 10)
-        self.fc3 = nn.Linear(10, 10)
-        self.fc4 = nn.Linear(10, 10)
-        self.fc5 = nn.Linear(10, 10)
-        self.fc6 = nn.Linear(10, 10)
-        self.fc7 = nn.Linear(10, 10)
-        self.fc8 = nn.Linear(10, 10)
-        self.fc9 = nn.Linear(10, 10)
-        self.fc10 = nn.Linear(10, 10)
-        self.fc11 = nn.Linear(10, 10)
-        self.fc12 = nn.Linear(10, 10)
-        self.fc13 = nn.Linear(10, 10)
-        self.fc14 = nn.Linear(10, 10)
-        self.fc15 = nn.Linear(10, 10)
-        self.fc16 = nn.Linear(10, 10)
-        self.fc17 = nn.Linear(10, 10)
-        self.fc18 = nn.Linear(10, 10)
-        self.fc19 = nn.Linear(10, 10)
-        self.fc20 = nn.Linear(10, 10)
-        self.fc21 = nn.Linear(10, 10)
-        self.fc22 = nn.Linear(10, 10)
-    
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
-        x = F.relu(self.fc5(x))
-        x = F.relu(self.fc6(x))
-        x = F.relu(self.fc7(x))
-        x = F.relu(self.fc8(x))
-        x = F.relu(self.fc9(x))
-        x = F.relu(self.fc10(x))
-        x = F.relu(self.fc11(x))
-        x = F.relu(self.fc12(x))
-        x = F.relu(self.fc13(x))
-        x = F.relu(self.fc14(x))
-        x = F.relu(self.fc15(x))
-        x = F.relu(self.fc16(x))
-        x = F.relu(self.fc17(x))
-        x = F.relu(self.fc18(x))
-        x = F.relu(self.fc19(x))
-        x = F.relu(self.fc20(x))
-        x = F.relu(self.fc21(x))
-        x = F.relu(self.fc22(x))
-        return x
-    
-# Training the model   
-def train(model, train_loader, optimizer, criterion, epoch):
-    model.train()
-    for batch_idx, data in enumerate(train_loader):
-        data = data.to(device)
-        optimizer.zero_grad()
-        output = model(data[:, :3])
-        loss = criterion(output, data[:, 3:])
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 100 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-    return loss.item()
+class MLP(torch.nn.Module):
+    def __init__(self, depth = 5, n_neurons_per_layer=8):
+        super(MLP, self).__init__()
+        self.depth = depth
+        self.loss = []
+        self.n_neurons_per_layer = n_neurons_per_layer
+        
+        # create an empty sequential model
+        self.Net = torch.nn.Sequential()
 
-# Testing the model
-def test(model, test_loader, criterion):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data in test_loader:
-            data = data.to(device)
-            output = model(data[:, :3])
-            test_loss += criterion(output, data[:, 3:]).item() # sum up batch loss
-    test_loss /= len(test_loader.dataset)
-    return test_loss
+        # Add first layer 
+        self.n_features = 3
+        self.n_labels = 3
+        self.Net.add_module("input_layer", torch.nn.Linear(self.n_features, self.n_neurons_per_layer))
+        
+        # Add hidden layers
+        for n_hidden_layer in range(self.depth):
+            self.Net.add_module(
+                "hidden_layer_num_{}".format(n_hidden_layer + 1),
+                torch.nn.Linear(self.n_neurons_per_layer, self.n_neurons_per_layer)
+            )
+            self.Net.add_module("hidden_activation_num_{}".format(n_hidden_layer + 1 ), torch.nn.ReLU())
+
+        # Add output layer
+        self.Net.add_module("output_layer", torch.nn.Linear(self.n_neurons_per_layer, self.n_labels))
+        
+        # Initialize weights iterating over all the modules
+        for m in self.Net.modules():
+            if isinstance(m, torch.nn.Linear):
+                torch.nn.init.xavier_uniform_(m.weight)
+                torch.nn.init.zeros_(m.bias)
+
+        # Define training loop 
+        self.optimizer = torch.optim.Adam(self.Net.parameters(), lr=0.001)
+        self.criterion = torch.nn.MSELoss()
+
+        # Define device
+        # self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.device = "cpu"
+
+    def train(self, train_loader, optimizer=None, criterion=None, num_epochs=2000):
+        if optimizer is None:
+            optimizer = self.optimizer
+        if criterion is None:
+            criterion = self.criterion
+
+        for epoch in range(num_epochs):
+            for batch_idx, data in enumerate(train_loader):
+                data = data.to(self.device)
+                optimizer.zero_grad()
+                predicted = self.Net(data[:, :3]) # Lx,E,p
+                ground_truths = data[:, :3] # Lx,E,p
+                loss = criterion(predicted, ground_truths)
+                loss.backward()
+                optimizer.step()
+                if batch_idx % 100 == 0:
+                    print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                        100. * batch_idx / len(train_loader), loss.item()))
+            
+            self.loss.append(loss.item())
+
+
+# # Testing the model
+# def test(model, test_loader, criterion):
+#     model.eval()
+#     test_loss = 0
+#     correct = 0
+#     with torch.no_grad():
+#         for data in test_loader:
+#             data = data.to(device)
+#             output = model(data[:, :3])
+#             test_loss += criterion(output, data[:, 3:]).item() # sum up batch loss
+#     test_loss /= len(test_loader.dataset)
+#     return test_loss
 
 # Main
 if __name__ == '__main__':
     # Loading the data
-    train_dataset = uniCompDataset('uniCompTrain.csv')
-    test_dataset = uniCompDataset('uniCompTest.csv')
+    train_dataset = uniCompDataset('results_2023-02-08_11-28-32.csv')
     train_loader = DataLoader(train_dataset, batch_size=1000, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=1000, shuffle=False)
-    
-    # Creating the model
-    model = uniCompNet()
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    
-    # Training the model
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-    start = time.time()
-    for epoch in range(1, 1000):
-        train(model, train_loader, optimizer, criterion, epoch)
-        test_loss = test(model, test_loader, criterion)
-        print('Test set: Average loss: {:.4f}'.format(test_loss))
-        
+
+    # Defining the model
+    mlp = MLP()
+    mlp.train(train_loader) 
+    pass
